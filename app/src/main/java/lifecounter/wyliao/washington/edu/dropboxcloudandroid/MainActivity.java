c92e5c330dd055935776bf67387f1af6b00b7b16
AbnerLiao <jim283312@gmail.com>
2015-06-08 05:17:39 -0700

    Drop-in function added

--- app/src/main/java/lifecounter/wyliao/washington/edu/dropboxcloudandroid/MainActivity.java
+++ app/src/main/java/lifecounter/wyliao/washington/edu/dropboxcloudandroid/MainActivity.java
@@ -1,17 +1,59 @@
 package lifecounter.wyliao.washington.edu.dropboxcloudandroid;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import com.dropbox.chooser.android.DbxChooser;
+
+
+
 
 
 public class MainActivity extends ActionBarActivity {
+
+    static final String APP_KEY = "zj1kedpdt1e5xmg";
+    static final int DBX_CHOOSER_REQUEST = 0;
+    private DbxChooser mChooser;
+    private Button DBBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mChooser = new DbxChooser(APP_KEY);
+        DBBtn = (Button) findViewById(R.id.db);
+        DBBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mChooser.forResultType(DbxChooser.ResultType.PREVIEW_LINK).launch(MainActivity.this,DBX_CHOOSER_REQUEST);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == DBX_CHOOSER_REQUEST) {
+            if (resultCode == MainActivity.RESULT_OK) {
+                DbxChooser.Result result = new DbxChooser.Result(data);
+                Log.v("Wen", "Link to selected file: " + result.getLink());
+                Log.v("Wen", "The file name you choose" + result.getName());
+
+            }
+            else {
+                Log.v("Wen", "Dropbox link error");
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 
